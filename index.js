7571e4a9d6041e6729fef27569b82f8a3d700454
Touhidur Zaman <touhidcodes@gmail.com>
2023-05-13 23:25:09 +0600

    connect-mongodb

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// coffeeMaster
+// QxssLBRWHgK2EFbx;
+
+const uri =
+	"mongodb+srv://coffeeMaster:QxssLBRWHgK2EFbx@cluster0.57whvd4.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+	serverApi: {
+		version: ServerApiVersion.v1,
+		strict: true,
+		deprecationErrors: true,
+	},
+});
+
+async function run() {
+	try {
+		// Connect the client to the server	(optional starting in v4.7)
+		await client.connect();
+		// Send a ping to confirm a successful connection
+		await client.db("admin").command({ ping: 1 });
+		console.log(
+			"Pinged your deployment. You successfully connected to MongoDB!"
+		);
+	} finally {
+		// Ensures that the client will close when you finish/error
+		// await client.close();
+	}
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+	res.send("Coffee Espresso Emporium Server is running......");
+});
+
+app.listen(port, () => {
+	console.log(`Coffee Espresso Emporium Server app listening on port ${port}`);
+});
