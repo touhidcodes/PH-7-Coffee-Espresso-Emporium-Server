0a0f7f38d1f502721852a2aa2d206f9059d8f81e
Touhidur Zaman <touhidcodes@gmail.com>
2023-05-14 16:07:15 +0600

    update-data-api

--- index.js
+++ index.js
@@ -50,6 +50,34 @@ async function run() {
 			res.send(result);
 		});
 
+		// Update data api
+		app.put("/coffee/:id", async (req, res) => {
+			const id = req.params.id;
+			const coffee = req.body;
+			const filter = { _id: new ObjectId(id) };
+			const options = { upsert: true };
+			const updateCoffee = {
+				$set: {
+					name: coffee.name,
+					chef: coffee.chef,
+					supplier: coffee.supplier,
+					taste: coffee.taste,
+					category: coffee.category,
+					details: coffee.details,
+					url: coffee.url,
+				},
+			};
+
+			const result = await coffeeCollection.updateOne(
+				filter,
+				updateCoffee,
+				options
+			);
+
+			res.send(result);
+		});
+
+		// Delete data api
 		app.delete("/coffee/:id", async (req, res) => {
 			const id = req.params.id;
 			const query = { _id: new ObjectId(id) };
